a955fc3c219573d761820a8244b4f5d5161cecd0
fsarachu <fsarachu@gmail.com>
2017-06-08 23:33:24 -0300

    Use arrow function syntax

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 let url = 'mongodb://localhost:27017/test';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
+MongoClient.connect(url, (err, db) => {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
