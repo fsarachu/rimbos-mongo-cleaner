972cb589296d1ddcf0786073101b0a24b7d1eee9
fsarachu <fsarachu@gmail.com>
2017-06-08 23:26:46 -0300

    Simple mongo connection

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`It's workin'`);
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+let url = 'mongodb://localhost:27017/test';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    db.close();
+});
