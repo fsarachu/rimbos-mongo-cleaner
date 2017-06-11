c2369c43c055882b8f849d650d3ca8a30c48d4f4
fsarachu <fsarachu@gmail.com>
2017-06-11 13:41:46 -0300

    Add mediaUrl attribute to eventPost model

--- models/eventPost.js
+++ models/eventPost.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
     'eventId': mongoose.Schema.Types.ObjectId,
+    'mediaUrl': String,
 });
 
 module.exports = mongoose.model('EventPost', schema, 'eventposts');
