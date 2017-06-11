208096188ff6ddd651db925ae46d1ca9796d8d96
fsarachu <fsarachu@gmail.com>
2017-06-11 12:07:20 -0300

    Rename Event.js to event.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 
 // Import stuff
 const mongoose = require('mongoose');
-const Event = require('./models/Event');
+const Event = require('./models/event');
 
 // Connect to DB
 mongoose.connect(process.env.DB_URL);
 
 // List all events
 Event.find({}, (err, events) => {
-    if(err) {
+    if (err) {
         console.error(err);
     }
 
-    for(let event of events) {
+    for (let event of events) {
         console.log(`${event._id}: ${event.code}`);
     }
 });
