8d6b734fe8cad6a6bfe54ddff24e980376f46486
fsarachu <fsarachu@gmail.com>
2017-06-11 03:56:32 -0300

    Sample index.js. Connects to db and fetches all events

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Load environment variables from .env
 require('dotenv').config();
 
+// Import stuff
 const mongoose = require('mongoose');
+const Event = require('./models/Event');
 
+// Connect to DB
 mongoose.connect(process.env.DB_URL);
+
+// List all events
+Event.find({}, (err, events) => {
+    if(err) {
+        console.error(err);
+    }
+
+    for(let event of events) {
+        console.log(`${event._id}: ${event.code}`);
+    }
+});
