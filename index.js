3fba81bdfccd7a5bd6daf0cc8463cef2b339bfae
fsarachu <fsarachu@gmail.com>
2017-06-11 22:24:18 -0300

    Define findEventPosts() function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Import stuff
 const mongoose = require('mongoose');
 const Event = require('./models/event');
+const EventPost = require('./models/eventPost');
 
 // Connect to DB
 mongoose.connect(process.env.DB_URL);
@@ -22,7 +23,7 @@ const eventIds = [
 findEvents(eventIds.map(id => mongoose.Types.ObjectId(id)));
 
 /**
- * Find the requested event documents.
+ * Find the requested events and their associated content.
  * @param {ObjectId[]} eventIds - An array of events ObjectId's.
  */
 function findEvents(eventIds) {
@@ -33,6 +34,27 @@ function findEvents(eventIds) {
 
         for (let event of events) {
             console.log(`Event: ${event.code} (${event.name})`);
+            findEventPosts(event._id);
         }
+    });
+}
+
+
+/**
+ * Finds all posts that belong to an event.
+ * @param {ObjectId} eventId - The event id.
+ */
+function findEventPosts(eventId) {
+    EventPost.find({eventId}, (err, posts) => {
+        if (err) {
+            console.error(err);
+        }
+
+        console.log(`  -- Found ${posts.length} posts --`);
+
+        for (let post of posts) {
+            console.log(`  Post: ${post._id} - ${post.mediaUrl}`);
+        }
+
     });
 }
