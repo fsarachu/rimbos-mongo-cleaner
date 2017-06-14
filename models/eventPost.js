6ec67c3449ab9bd90d04bceaf055a43c77002802
fsarachu <fsarachu@gmail.com>
2017-06-13 22:17:46 -0300

    Update logger creation on eventPost.js

--- models/eventPost.js
+++ models/eventPost.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const PostComment = require('./postComment');
-const log = require('../helpers/log');
+const log = require('simple-node-logger').createSimpleLogger({
+    logFilePath: './log/EventPost.log',
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
+});
 
 const eventPostSchema = new mongoose.Schema({
     'eventId': mongoose.Schema.Types.ObjectId,
     'mediaUrl': String,
 });
 
-eventPostSchema.pre('remove', function(next) {
+eventPostSchema.pre('remove', function (next) {
     PostComment.find({eventPostId: this._id}, (err, comments) => {
-        if(err) {
+        if (err) {
             log.error(err);
         }
 
