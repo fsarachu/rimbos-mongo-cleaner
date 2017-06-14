35cef65b6f9ab6a73f442c5e1e83ff7456e097f6
fsarachu <fsarachu@gmail.com>
2017-06-13 22:22:04 -0300

    Add post remove hook on "PostComment" model

--- models/postComment.js
+++ models/postComment.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const log = require('simple-node-logger').createSimpleLogger({
+    logFilePath: './log/PostComment.log',
+    timestampFormat: 'YYYY-MM-DD HH:mm:ss.SSS',
+});
 
 const schema = new mongoose.Schema({
     'eventPostId': mongoose.Schema.ObjectId,
     'text': String,
 });
+
+eventPostSchema.post('remove', removed => log.info(`Post ${removed._id} has been removed`));
 
 module.exports = mongoose.model('PostComment', schema, 'postcomments');
