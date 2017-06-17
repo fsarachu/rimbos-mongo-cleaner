a4c066e3468aeec48b239505225aaf073cd754c3
fsarachu <fsarachu@gmail.com>
2017-06-16 22:06:04 -0300

    Reformat file

--- functions.js
+++ functions.js
@@ -21,6 +21,7 @@ mongoose.Promise = global.Promise;
  */
 function findEvents(eventIds) {
     Event.find({_id: {$in: eventIds}}, (err, events) => {
+
         if (err) {
             log.error(err);
         }
@@ -28,6 +29,7 @@ function findEvents(eventIds) {
         log.info(`${events.length} Events Found`);
 
         findEventPosts(events.map(e => e._id));
+
     });
 }
 
@@ -37,7 +39,9 @@ function findEvents(eventIds) {
  * @param {ObjectId[]} eventIds - An array of events ObjectId's.
  */
 function findEventPosts(eventIds) {
+
     EventPost.find({eventId: {$in: eventIds}}, (err, posts) => {
+
         if (err) {
             log.error(err);
         }
@@ -45,7 +49,9 @@ function findEventPosts(eventIds) {
         log.info(`${posts.length} Posts Found`);
 
         findPostComments(posts.map(p => p._id));
+
     });
+
 }
 
 
@@ -54,12 +60,15 @@ function findEventPosts(eventIds) {
  * @param {ObjectId[]} postIds - An array of event post ObjectId's.
  */
 function findPostComments(postIds) {
+
     PostComment.find({eventPostId: {$in: postIds}}, (err, comments) => {
+
         if (err) {
             log.error(err);
         }
 
         log.info(`${comments.length} Comments Found`);
+
     });
 }
 
@@ -69,7 +78,9 @@ function findPostComments(postIds) {
  * @param {ObjectId[]} eventIds - An array of events ObjectId's.
  */
 function deleteEvents(eventIds) {
+
     Event.find({_id: {$in: eventIds}}, (err, events) => {
+
         if (err) {
             log.error(err);
         }
@@ -77,23 +88,33 @@ function deleteEvents(eventIds) {
         for (let event of events) {
             event.remove();
         }
+
     });
+
 }
+
 
 /**
  * Finds event posts that belongs to a non existing event.
  */
 function findOrphanPosts() {
+
     let allPosts = EventPost.find().exec();
+
     allPosts.then(posts => {
+
         for (let post of posts) {
+
             let event = Event.findById(post.eventId).exec();
+
             event.then(event => {
                 if (!event) {
                     console.log(`${post._id} is orphan. It belongs to the event ${post.eventId}`);
                 }
             });
+
         }
+
     })
 }
 
