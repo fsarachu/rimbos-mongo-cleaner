25838b091a30f3c36286a29d01ac05610cf3e1f5
fsarachu <fsarachu@gmail.com>
2017-06-19 20:15:23 -0300

    Define "User" mongoose model

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    fistname: String,
+    lastname: String,
+    email: String,
+    userRelationship: String,
+    facebookId: Number,
+    profilePicture: String,
+    gender: String,
+    ageRange: String,
+    archived: Boolean,
+    role: String,
+    facebookSessionToken: String,
+    facebookExpirationTime: Number,
+    facebookLastTimeRefreshed: Number,
+    sessionToken: String,
+    eventsUsed: Number,
+    eventsAvailable: Number,
+    phone: String,
+    organizationName: String,
+    category: String,
+    country: mongoose.Schema.Types.ObjectId,
+    sections: [mongoose.Schema.Types.ObjectId],
+    createdAt: Date,
+    updatedAt: Date
+});
+
+module.exports = mongoose.model('User', userSchema, 'users');
